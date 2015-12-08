e461c7af5030ceb9ab6aff689273967a2d858e6f
puvowar <puvowar@gmail.com>
2015-12-08 17:47:15 +0200

    add formattedAddress to Location clean unused using in Location update summary in Venue.location

--- Entities/Venue.cs
+++ Entities/Venue.cs
@@ -35,7 +35,7 @@ namespace FourSquare.SharpSquare.Entities
         }
 
         /// <summary>
-        ///  An object containing none, some, or all of address (street address), crossStreet, city, state, postalCode, country, lat, lng, and distance. All fields are strings, except for lat, lng, and distance. Distance is measured in meters. 
+        ///  An object containing none, some, or all of address (street address), formattedAddress, crossStreet, city, state, postalCode, country, lat, lng, and distance. All fields are strings, except for lat, lng, and distance. Distance is measured in meters. 
         ///  Some venues have their locations intentionally hidden for privacy reasons (such as private residences). If this is the case, the parameter isFuzzed will be set to true, and the lat/lng parameters will have reduced precision. 
         /// </summary>
         public Location location
