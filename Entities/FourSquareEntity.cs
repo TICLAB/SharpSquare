a31a1f6faad1088cb7a717c1616164484f31646d
puvowar <puvowar@gmail.com>
2016-07-18 10:43:05 +0300

    refactoring

--- Entities/FourSquareEntity.cs
+++ Entities/FourSquareEntity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace FourSquare.SharpSquare.Entities
+﻿namespace FourSquare.SharpSquare.Entities
 {
     public abstract class FourSquareEntity
     {
--- Entities/Icon.cs
+++ Entities/Icon.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace FourSquare.SharpSquare.Entities
+﻿namespace FourSquare.SharpSquare.Entities
 {
     public class Icon : FourSquareEntity
     {
